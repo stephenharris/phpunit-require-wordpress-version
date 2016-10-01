5a42918d2e088781c00b2b91deb930ce52c10f03
Stephen Harris <contact@stephenharris.info>
2016-10-01 16:49:14 +0100

    Additional tests & refactoring.

--- src/RequiresWordPressVersion.php
+++ src/RequiresWordPressVersion.php
@@ -4,58 +4,60 @@ namespace StephenHarris\PHPUnit;
 
 trait RequiresWordPressVersion {
 
-    protected $operators = array(
-        '<' => '<',
-        'lt' => '<',
-        '<=' => '<=',
-        'le' => '<=',
-        '>' => '>',
-        'gt' => '>',
-        '>=' => '>=',
-        'ge' => '>=',
-        '==' => '==',
-        '=' => '==',
-        'eq' => '==',
-        '!=' => '!=',
-        '<>' => '!=',
-        'ne' => '!=',
-    );
+  protected $operators = array(
+    '<' => '<',
+    'lt' => '<',
+    '<=' => '<=',
+    'le' => '<=',
+    '>' => '>',
+    'gt' => '>',
+    '>=' => '>=',
+    'ge' => '>=',
+    '==' => '==',
+    '=' => '==',
+    'eq' => '==',
+    '!=' => '!=',
+    '<>' => '!=',
+    'ne' => '!=',
+  );
 
-    protected function checkRequirements() {
-        parent::checkRequirements();
+  protected function checkRequirements() {
+    parent::checkRequirements();
 
-        $annotations = $this->getAnnotations();
+	  $annotations = $this->getAnnotations();
 
-        if ( empty($annotations) ) {
-            return;
-        }
+    if ( empty($annotations) ) {
+        return;
+    }
 
-        //$this->getAnnotations returns an array indexed by 'class' and 'method'
-        //with annotations for the class and method respectively. We don't care about the location of the annotation
-        //so we just merge them:
-        $annotations = array_merge( $annotations['class'], $annotations['method'] );
+    //$this->getAnnotations returns an array indexed by 'class' and 'method'
+    //with annotations for the class and method respectively. We don't care about the location of the annotation
+    //so we just merge them:
+    $annotations = array_merge( $annotations['class'], $annotations['method'] );
 
-        if ( empty($annotations['requires']) ) {
-            return;
-        }
+      if ( empty($annotations['requires']) ) {
+        return;
+      }
 
-        foreach ( $annotations['requires'] as $required ) {
-            if ( $target = $this->parseRequiresWordPressVersion( $required ) ) {
-                if ( ! version_compare( $this->getWordPressVersion(), $target['version'], $target['operator'] ) ) {
+      foreach ( $annotations['requires'] as $required ) {
+        if ( $target = $this->parseRequiresWordPressVersion( $required ) ) {
+          if ( ! version_compare( $this->getWordPressVersion(), $target['version'], $target['operator'] ) ) {
 
-                    $message = sprintf(
-                        'Requires WordPress %s %s; Running %s.',
-                        $this->translateVersionOperator( $target['operator'] ),
-                        $target['version'],
-                        $this->getWordPressVersion()
-                    );
-                    if ( $target['message'] ) {
-                        $message .= "\n" . $target['message'];
-                    }
-                    $this->markTestSkipped( $message );
-                }
+            $message = sprintf(
+              'Requires WordPress %s %s; Running %s.',
+              $this->translateVersionOperator( $target['operator'] ),
+              $target['version'],
+              $this->getWordPressVersion()
+            );
+
+            if ( $target['message'] ) {
+                $message .= "\n" . $target['message'];
             }
-        }
+
+            $this->markTestSkipped( $message );
+          }
+      	}
+  		}
     }
 
     /**
@@ -64,29 +66,32 @@ trait RequiresWordPressVersion {
      * @return Normalised WordPress version
      */
     protected function getWordPressVersion() {
-
-        $wp_version = get_bloginfo( 'version' );
-
-        if ( preg_match( '/^\d+\.\d+$/', $wp_version ) ) {
-            $wp_version .= '.0';
-        }
-
-        return $wp_version;
+      $wp_version = $this->padVersion( get_bloginfo( 'version' ) );
+      return $wp_version;
     }
 
+		protected function padVersion( $version ) {
+			$version = preg_replace( '/^(\d+\.\d+)(-.*)?$/', '$1.0$2', $version );
+			return $version;
+		}
+
     protected function parseRequiresWordPressVersion( $string ) {
-        preg_match( '/WordPress (?P<operator><|lt|<=|le|>|gt|>=|ge|==|=|eq|!=|<>|ne)?\s*(?P<version>\d+\.\d+(\.\d+)?(-(stable|beta|b|RC|alpha|a|patch|pl|p))?)\s*(?P<message>.*)?/', $string, $matches );
+      preg_match( '/WordPress (?P<operator><|lt|<=|le|>|gt|>=|ge|==|=|eq|!=|<>|ne)?\s*(?P<version>\d+\.\d+(\.\d+)?(-(stable|beta|b|RC|alpha|a|patch|pl|p)(-\d+)?)?)\s*(?P<message>.*)?/i', $string, $matches );
 
-        if ( ! $matches ) {
-            return;
-        }
+      if ( ! $matches ) {
+        return;
+      }
 
-        $operator = ! empty( $matches['operator'] ) ? $matches['operator'] : '>=';
-        return array( 'version' => $matches['version'], 'operator' => $operator, 'message' => $matches['message'] );
+      $operator = ! empty( $matches['operator'] ) ? $matches['operator'] : '>=';
+      return array(
+				'version'  => $this->padVersion( $matches['version'] ),
+				'operator' => $operator,
+				'message'  => $matches['message']
+			);
     }
 
     protected function translateVersionOperator( $operator ) {
-        return $this->operators[ $operator ];
+      return $this->operators[ $operator ];
     }
 
 }
--- tests/ParsingRequiresStringTest.php
+++ tests/ParsingRequiresStringTest.php
@@ -1,5 +1,4 @@
 <?php
-
 use MonkeryTestCase\BrainMonkeyWpTestCase;
 use Brain\Monkey\Functions;
 
@@ -8,34 +7,27 @@ class ParsingRequiresStringTest extends BrainMonkeyWpTestCase {
     function setUp() {
         parent::setUp();
         $this->test_subject = new mockTestCase();
-
     }
 
     public function testNoAnnotations() {
         $this->test_subject->triggerCheckRequirements();
     }
 
-
     /**
      * @dataProvider requiresStringFailsProvider
      */
     public function testWordPressVersionRequiredButFails( $requires, $wp_version, $exception ) {
 
-        Functions::expect('get_bloginfo')
-            ->zeroOrMoreTimes()
+        Functions::expect('get_bloginfo')->zeroOrMoreTimes()
             ->with('version')
             ->andReturn( $wp_version );
 
-        $this->test_subject->_setAnnotations(
-            array(
-                'class' => array(),
-                'method' => array(
-                    'requires' => array(
-                        $requires
-                    ),
-                )
-            )
-        );
+        $this->test_subject->_setAnnotations( array(
+          'class' => array(),
+          'method' => array(
+              'requires' => array( $requires ),
+          )
+        ));
 
         $this->setExpectedException( \Exception::class, $exception );
 
@@ -44,43 +36,42 @@ class ParsingRequiresStringTest extends BrainMonkeyWpTestCase {
     }
 
     public function requiresStringFailsProvider() {
-        return array(
+      return array(
+        //Greater than or equal to
+        'no operator' => array( "WordPress 4.5.1", "4.5", "Requires WordPress >= 4.5.1; Running 4.5.0." ),
+        '>= operator' => array( "WordPress >= 4.5.1", "4.5", "Requires WordPress >= 4.5.1; Running 4.5.0." ),
+        'ge operator' => array( "WordPress ge 4.5.1", "4.5", "Requires WordPress >= 4.5.1; Running 4.5.0." ),
+        'greater or equal than alpha' => array( "WordPress >= 4.5.1-alpha", "4.5.0", "Requires WordPress >= 4.5.1-alpha; Running 4.5.0." ),
+      	'alpha greater or equal than' => array( "WordPress >= 4.5.0", "4.5-alpha-123", "Requires WordPress >= 4.5.0; Running 4.5.0-alpha-123." ),
+        'require alpha 2' => array( "WordPress 4.5.1-alpha-2", "4.5.1-alpha-1", "Requires WordPress >= 4.5.1-alpha-2; Running 4.5.1-alpha-1." ),
+        'require rc' => array( "WordPress 4.5.1-rc-2", "4.5.1-alpha-5", "Requires WordPress >= 4.5.1-rc-2; Running 4.5.1-alpha-5." ),
 
-            //Greater than or equal to
-            'no operator' => array( "WordPress 4.5.1", "4.5.0", "Requires WordPress >= 4.5.1; Running 4.5.0." ),
-            '>= operator' => array( "WordPress >= 4.5.1", "4.5.0", "Requires WordPress >= 4.5.1; Running 4.5.0." ),
-            'ge operator' => array( "WordPress ge 4.5.1", "4.5.0", "Requires WordPress >= 4.5.1; Running 4.5.0." ),
-            'greater or equal than alpha' => array( "WordPress >= 4.5.1-alpha", "4.5.0", "Requires WordPress >= 4.5.1-alpha; Running 4.5.0." ),
-            'alpha greater or equal than' => array( "WordPress >= 4.5.0", "4.5.0-alpha-123", "Requires WordPress >= 4.5.0; Running 4.5.0-alpha-123." ),
+        //Greater than
+        '> operator' => array( "WordPress > 4.5.1", "4.5.1", "Requires WordPress > 4.5.1; Running 4.5.1." ),
+        'gt operator' => array( "WordPress gt 4.5.1", "4.5.1", "Requires WordPress > 4.5.1; Running 4.5.1." ),
+        'greater than alpha' => array( "WordPress > 4.5.1-alpha", "4.5.1-alpha", "Requires WordPress > 4.5.1-alpha; Running 4.5.1-alpha." ),
 
-            //Greater than
-            '> operator' => array( "WordPress > 4.5.1", "4.5.1", "Requires WordPress > 4.5.1; Running 4.5.1." ),
-            'gt operator' => array( "WordPress gt 4.5.1", "4.5.1", "Requires WordPress > 4.5.1; Running 4.5.1." ),
-            'greater than alpha' => array( "WordPress > 4.5.1-alpha", "4.5.1-alpha", "Requires WordPress > 4.5.1-alpha; Running 4.5.1-alpha." ),
+        //Equal to
+        '== operator' => array( "WordPress == 4.5.1", "4.5.1-alpha", "Requires WordPress == 4.5.1; Running 4.5.1-alpha." ),
+        '= operator' => array( "WordPress == 4.5.1-alpha", "4.5.1", "Requires WordPress == 4.5.1-alpha; Running 4.5.1." ),
+        'eq operator' => array( "WordPress == 4.5.1", "4.5.2", "Requires WordPress == 4.5.1; Running 4.5.2." ),
+        'unequal alphas' => array( "WordPress == 4.5.1-alpha-1", "4.5.1-alpha-2", "Requires WordPress == 4.5.1-alpha-1; Running 4.5.1-alpha-2." ),
 
-            //Equal to
-            '== operator' => array( "WordPress == 4.5.1", "4.5.1-alpha", "Requires WordPress == 4.5.1; Running 4.5.1-alpha." ),
-            '= operator' => array( "WordPress == 4.5.1-alpha", "4.5.1", "Requires WordPress == 4.5.1-alpha; Running 4.5.1." ),
-            'eq operator' => array( "WordPress == 4.5.1", "4.5.2", "Requires WordPress == 4.5.1; Running 4.5.2." ),
-            //'unequal alphas' => array( "WordPress == 4.5.1-alpha-1", "4.5.1-alpha-2", "Requires WordPress == 4.5.1-alpha-1; Running 4.5.1-alpha-2." ),
+        //Not equal to
+      	'!= operator' => array( "WordPress != 4.5.1", "4.5.1", "Requires WordPress != 4.5.1; Running 4.5.1." ),
+        'ne operator' => array( "WordPress ne 4.5.1", "4.5.1", "Requires WordPress != 4.5.1; Running 4.5.1." ),
+        '<> operator' => array( "WordPress <> 4.5.1", "4.5.1", "Requires WordPress != 4.5.1; Running 4.5.1." ),
 
-            //Not equal to
-            '!= operator' => array( "WordPress != 4.5.1", "4.5.1", "Requires WordPress != 4.5.1; Running 4.5.1." ),
-            'ne operator' => array( "WordPress ne 4.5.1", "4.5.1", "Requires WordPress != 4.5.1; Running 4.5.1." ),
-            '<> operator' => array( "WordPress <> 4.5.1", "4.5.1", "Requires WordPress != 4.5.1; Running 4.5.1." ),
+        //Less than
+        '< operator' => array( "WordPress < 4.5.1", "4.5.1", "Requires WordPress < 4.5.1; Running 4.5.1." ),
+        'l operator' => array( "WordPress lt 4.5.1", "4.5.1", "Requires WordPress < 4.5.1; Running 4.5.1." ),
+        'less than alpha' => array( "WordPress < 4.5.1-alpha", "4.5.1-alpha", "Requires WordPress < 4.5.1-alpha; Running 4.5.1-alpha." ),
 
-            //Less than or equal to
-            '<= operator' => array( "WordPress <= 4.5.1", "4.5.2", "Requires WordPress <= 4.5.1; Running 4.5.2." ),
-            'le operator' => array( "WordPress le 4.5.1", "4.5.2", "Requires WordPress <= 4.5.1; Running 4.5.2." ),
-            'less than alpha' => array( "WordPress <= 4.5.1-alpha", "4.5.1", "Requires WordPress <= 4.5.1-alpha; Running 4.5.1." ),
-
-            //Less than or equal to
-            '<= operator' => array( "WordPress <= 4.5.1", "4.5.2", "Requires WordPress <= 4.5.1; Running 4.5.2." ),
-            'le operator' => array( "WordPress le 4.5.1", "4.5.2", "Requires WordPress <= 4.5.1; Running 4.5.2." ),
-            'less than or equal alpha' => array( "WordPress <= 4.5.1-alpha", "4.5.1", "Requires WordPress <= 4.5.1-alpha; Running 4.5.1." ),
-
-
-        );
+        //Less than or equal to
+        '<= operator' => array( "WordPress <= 4.5.1", "4.5.2", "Requires WordPress <= 4.5.1; Running 4.5.2." ),
+        'le operator' => array( "WordPress le 4.5.1", "4.5.2", "Requires WordPress <= 4.5.1; Running 4.5.2." ),
+        'less than or equal alpha' => array( "WordPress <= 4.5.1-alpha", "4.5.1", "Requires WordPress <= 4.5.1-alpha; Running 4.5.1." ),
+      );
     }
 
     /**
@@ -88,32 +79,157 @@ class ParsingRequiresStringTest extends BrainMonkeyWpTestCase {
      */
     public function testWordPressVersionRequiredButPasses( $requires, $wp_version ) {
 
-        Functions::expect('get_bloginfo')
-            ->zeroOrMoreTimes()
-            ->with('version')
-            ->andReturn( $wp_version );
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+					->with('version')
+					->andReturn( $wp_version );
 
-        $this->test_subject->_setAnnotations(
-            array(
-                'class' => array(),
-                'method' => array(
-                    'requires' => array(
-                        $requires
-                    ),
-                )
-            )
-        );
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+						'requires' => array( $requires ),
+				)
+			));
 
-        $this->test_subject->triggerCheckRequirements();
-
+      $this->test_subject->triggerCheckRequirements();
     }
 
     public function requiresStringPassesProvider() {
         return array(
             'identical version' => array( "WordPress 4.5.1", "4.5.1" ),
             'simple case' => array( "WordPress 4.5.1", "4.5.2" ),
-            'missing patch version' => array( "WordPress 4.5.0", "4.5" ),
+            'alpha < beta' => array( "WordPress > 4.5.1-alpha-5", "4.5.1-beta-1" ),
+						'alpha < RC' => array( "WordPress > 4.5.1-alpha-5", "4.5.1-rc-1" ),
+						'alpha 1 < alpha 2' => array( "WordPress > 4.5.1-alpha-1", "4.5.1-alpha-2" ),
+						'0 patch optional' => array( "WordPress == 4.5", "4.5" ),
         );
     }
+
+
+		/**
+		 * @expectedException Exception
+		 * @expectedExceptionMessage Requires WordPress >= 4.4.0; Running 4.3.0.
+		 *                           Tested behaviour requires term meta data
+		 */
+		public function testCustomMessage() {
+
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+					->with('version')
+					->andReturn( '4.3' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+						'requires' => array( 'WordPress 4.4.0 Tested behaviour requires term meta data' ),
+				)
+			));
+
+      $this->test_subject->triggerCheckRequirements();
+    }
+
+		public function testVersionCaseInsensitive() {
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+					->with('version')
+					->andReturn( '4.3-alpha-1234' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+						'requires' => array( 'WordPress == 4.3-aLPhA-1234' ),
+				)
+			));
+
+			$this->test_subject->triggerCheckRequirements();
+		}
+
+		/**
+		 * @expectedException Exception
+		 * @expectedExceptionMessage Requires WordPress >= 4.4.0; Running 4.3.0.
+		 */
+		public function testWordPressCaseInsensitive() {
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+					->with('version')
+					->andReturn( '4.3' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+						'requires' => array( 'Wordpress 4.4.0' ),
+				)
+			));
+
+			$this->test_subject->triggerCheckRequirements();
+		}
+
+		/**
+		 * @expectedException Exception
+		 * @expectedExceptionMessage Requires WordPress != 4.3.0; Running 4.3.0.
+		 */
+		public function testWithout0PatchVersion() {
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+				->with('version')
+				->andReturn( '4.3' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+						'requires' => array( 'Wordpress != 4.3' ),
+				)
+			));
+			$this->test_subject->triggerCheckRequirements();
+		}
+
+		/**
+		 * @expectedException Exception
+		 * @expectedExceptionMessage Requires WordPress != 4.3.0; Running 4.3.0.
+		 */
+		public function testWith0PatchVersion() {
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+				->with('version')
+				->andReturn( '4.3' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+						'requires' => array( 'Wordpress != 4.3.0' ),
+				)
+			));
+			$this->test_subject->triggerCheckRequirements();
+		}
+
+		/**
+		 * @expectedException Exception
+		 * @expectedExceptionMessage Requires WordPress != 4.3.0-alpha-123; Running 4.3.0-alpha-123.
+		 */
+		public function testPatchVersionWithout0PatchAndAlpha() {
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+				->with('version')
+				->andReturn( '4.3-alpha-123' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+					'requires' => array( 'Wordpress != 4.3-alpha-123' ),
+				)
+			));
+			$this->test_subject->triggerCheckRequirements();
+		}
+
+		/**
+		 * @expectedException Exception
+		 * @expectedExceptionMessage Requires WordPress != 4.3.0-alpha-123; Running 4.3.0-alpha-123.
+		 */
+		public function testPatchVersionWith0PatchAndAlpha() {
+			Functions::expect('get_bloginfo')->zeroOrMoreTimes()
+				->with('version')
+				->andReturn( '4.3-alpha-123' );
+
+			$this->test_subject->_setAnnotations( array(
+				'class' => array(),
+				'method' => array(
+					'requires' => array( 'Wordpress != 4.3.0-alpha-123' ),
+				)
+			));
+			$this->test_subject->triggerCheckRequirements();
+		}
 
 }
